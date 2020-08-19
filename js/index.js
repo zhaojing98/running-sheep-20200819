6ad0a71f77b011c9818f71be4f39c620afaf3026
zhaojing <zj595920@163.com>
2020-08-19 22:46:20 +0800

    奔跑的小羊

--- js/index.js
+++ js/index.js
@@ -8,12 +8,6 @@
  * 2，多只羊出现舞台上  如何实现？每只羊的速度不一样  ？思考     √
  * 
  * 3，拖拽的时候   动画的交互                                 √                            
- * 
- * 
- * 
- * demo的制作就完成了 
- * 
- * 
  */
 
 
@@ -78,7 +72,7 @@ var sheeps = {
 
         //拖拽部分
 
-        sheepPlus.sheep.onmousedown  = function(e){
+        sheepPlus.sheep.onmousedown = function (e) {
             sheepPlus.speed = 0;//速度停止
             sheepPlus.top = -128;
 
@@ -87,8 +81,8 @@ var sheeps = {
             sheepPlus.x = e.pageX;
             sheepPlus.y = e.pageY;
 
-            document.onmousemove =  function(e){
-                var chax =  e.pageX - sheepPlus.x;
+            document.onmousemove = function (e) {
+                var chax = e.pageX - sheepPlus.x;
                 var chay = e.pageY - sheepPlus.y;
                 sheepPlus.sheep.style.left = chax + sheepPlus.sheep.offsetLeft + 'px';
                 sheepPlus.sheep.style.top = chay + sheepPlus.sheep.offsetTop + 'px';
@@ -97,7 +91,7 @@ var sheeps = {
 
             }
 
-            document.onmouseup = function(){
+            document.onmouseup = function () {
                 sheepPlus.speed = sheepPlus.currentSpeed;//还原速度
                 sheepPlus.top = 0;
                 document.onmousemove = null;
@@ -123,22 +117,7 @@ sheeps.init();//入口函数
 //前端一定需要会 
 // 懂一门后端语言  nodejs
 
-//两个阶段
-// 1，就业班
-// 2，进阶版
 
-// 报名全阶班的同学     签约就业保障协议 10w  ；
-                   //  该阶段与时俱进的课程更新
-                   //  大厂内推的名额
-                   //  IT人脉圈
-                   //  折扣  曾送一套书 
-
-                   //  原价 12999    -----  折扣   大家 问一下 自己的顾问 
-
-                   //  线上 ：直播  + 录播相结合的学习方式  
-                   //  线上答疑天团   
-
-                      
 
 
 
